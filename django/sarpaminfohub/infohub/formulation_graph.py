63862db156f54d4511b8fefbd35a170f0ff76453
PA Parent <paparent@paparent.me>
2011-03-07 13:56:45 -0500

    Update graph according to Martin comments. Redmine #679
    - Graph has been separated from FormulationTable to FormulationTable
    
    The width calculation is still in the template for now. we use % for
    now, but is the design change, we can only update the template for that.
    
    - Graph is a little more compact, and two differents colors for the
      bars.
    
    It have now the price in text in it.

--- django/sarpaminfohub/infohub/formulation_graph.py
+++ django/sarpaminfohub/infohub/formulation_graph.py
@@ -2,19 +2,27 @@ import django_tables as tables
 from sarpaminfohub.infohub.sarpam_table import SarpamTable
 from django.template.loader import render_to_string
 
-class FormulationTable(SarpamTable):
+class FormulationGraph(SarpamTable):
     country = tables.Column()
     fob_price = tables.Column(verbose_name="FOB Price")
     landed_price = tables.Column(verbose_name="Landed Price")
-    rows_template = "formulation_rows.html"
+    max_price = 0
+    NO_DATA = ""
 
     def __init__(self, rows):
         for row in rows:
             self.round_to_three_decimal_places(row, 'fob_price')
             self.round_to_three_decimal_places(row, 'landed_price')
 
+            if isinstance(row['fob_price'], float) and row['fob_price'] > self.max_price:
+                self.max_price = row['fob_price']
+
+            if isinstance(row['landed_price'], float) and row['landed_price'] > self.max_price:
+                self.max_price = row['landed_price']
+
         tables.MemoryTable.__init__(self, rows)
 
     def as_html(self):
-        return render_to_string('table.html',\
+        return render_to_string('formulation/graph.html',\
                                 {'table':self})
+
