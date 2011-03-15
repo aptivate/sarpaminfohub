4c2f6beff34013e550b4115e67efb9e3952170cc
Gemma Hentsch <rassilon2467@gmail.com>
2011-03-15 14:48:58 +0000

    more flexible naming

--- django/sarpaminfohub/contactlist/models.py
+++ django/sarpaminfohub/contactlist/models.py
@@ -1,8 +1,21 @@
 from django.db import models
 
+TITLES = (
+(1, 'Dr'),
+(1, 'Honorable'),
+(1, 'Mr'),
+(1, 'Ms'),
+(1, 'Miss'),
+(1, 'Mrs'),
+(1, 'Sir'),
+(1, 'Madam'),
+)
+
 class Contact(models.Model):
+    title = models.CharField(length=1,choices=TITLES)
     given_name = models.CharField(max_length=128)
     family_name = models.CharField(max_length=128)
+    additional_family_name = models.CharField(max_length=128)
     phone = models.CharField(max_length=20)
     email = models.CharField(max_length=256)
     address = models.CharField(max_length=512)
