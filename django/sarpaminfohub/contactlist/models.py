0a88ff5ddf8f72e3d67af7e00dfbd84828197406
Gemma Hentsch <rassilon2467@gmail.com>
2011-03-15 14:33:41 +0000

    Switching to given name and family name

--- django/sarpaminfohub/contactlist/models.py
+++ django/sarpaminfohub/contactlist/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Contact(models.Model):
-    first_name = models.CharField(max_length=128)
-    last_name = models.CharField(max_length=128)
+    given_name = models.CharField(max_length=128)
+    family_name = models.CharField(max_length=128)
     phone = models.CharField(max_length=20)
     email = models.CharField(max_length=256)
     address = models.CharField(max_length=512)
