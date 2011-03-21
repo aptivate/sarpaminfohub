565fdeab7be07a5add69c2a325af78f99d9ae483
Gemma Hentsch <rassilon2467@gmail.com>
2011-03-21 13:35:49 +0000

    multiple choice model field in searchform

--- django/sarpaminfohub/contactlist/forms.py
+++ django/sarpaminfohub/contactlist/forms.py
@@ -4,5 +4,5 @@ from tagging.models import Tag
 
 class SearchForm(forms.Form):
     search_term = forms.CharField(required=True)
-    tag = forms.ModelChoiceField(required=False,queryset=Tag.objects.all())
+    tag = forms.ModelMultipleChoiceField(required=False,queryset=Tag.objects.all())
     
