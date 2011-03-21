ab4ac1c5816707c498e3f3d9e0356cb75e742be8
Gemma Hentsch <rassilon2467@gmail.com>
2011-03-21 00:30:13 +0000

    adding in a basic id based view for contacts

--- django/sarpaminfohub/contactlist/urls.py
+++ django/sarpaminfohub/contactlist/urls.py
@@ -1,4 +1,11 @@
 from django.conf.urls.defaults import *
+from django.views.generic import list_detail
+from contactlist.models import Contact
+
+contacts_info = {
+    'queryset': Contact.objects.all()
+}
 urlpatterns = patterns('',
-    (r'^search/', include('haystack.urls')),
+    (r'^$', include('haystack.urls')),
+    (r'^(?P<object_id>\d+)/$', list_detail.object_detail, contacts_info)
 )
