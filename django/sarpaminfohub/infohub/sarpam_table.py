2cab36064eaee100ecf6f36ba73f52631b2f4dd5
Gemma Hentsch <rassilon2467@gmail.com>
2011-03-22 19:08:04 +0000

    fixes formatting to 3 decimal places

--- django/sarpaminfohub/infohub/sarpam_table.py
+++ django/sarpaminfohub/infohub/sarpam_table.py
@@ -14,7 +14,9 @@ class SarpamTable(tables.MemoryTable):
         if value is None:
             value = self.NO_DATA
         else:
-            value = round(float(value), 3)
+            # value = round(float(value), 3)
+            #value = round(float(value), 3)
+            value = "%.3f"%value
          
         row[column] = value
 
