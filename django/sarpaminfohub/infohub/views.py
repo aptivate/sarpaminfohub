e09a74b75681229b74eda356a4f05db0b811f907
Gemma Hentsch <rassilon2467@gmail.com>
2011-03-29 08:42:18 +0100

    last changes for martin

--- django/sarpaminfohub/infohub/views.py
+++ django/sarpaminfohub/infohub/views.py
@@ -70,7 +70,7 @@ def formulation(request, formulation_id, backend_name="django"):
     formulation_tab = get_formulation_tab(None)
     products_tab = get_products_tab(products_href)
     menu = Menu([formulation_tab, products_tab])
-    #temporary input
+    #this is what you need to edit to put in the correct values - Gemma to Martin
     formulation_pack = []
     for pack in drug_searcher.get_prices_for_formulation_with_id(formulation_id):
         temp_pack = [str(pack_val) for pack_val in pack.values()]
