b1c8e69e8893e5fc9162ec98bc7f16e23f241dda
Martin Burchell <martinb@aptivate.org>
2011-05-25 09:48:42 +0100

    Fixed wildcard imports

--- django/sarpaminfohub/infohub/admin.py
+++ django/sarpaminfohub/infohub/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from models import *
+from sarpaminfohub.infohub.models import Formulation, Incoterm, Manufacturer,\
+    Supplier, Price, Country, ExchangeRate, MSHPrice, Product,\
+    ProductRegistration
 
 admin.site.register(Formulation)
 admin.site.register(Country)
