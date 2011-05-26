387ab4f26b7c64318847c51d66b5845b53aff23f
Martin Burchell <martinb@aptivate.org>
2011-05-26 16:25:21 +0100

    Fixed some pylint violations

--- django/sarpaminfohub/contactlist/tests/__init__.py
+++ django/sarpaminfohub/contactlist/tests/__init__.py
@@ -1,7 +1,7 @@
 # pylint: disable-msg=W0401
-from contact_detail_page_tests import *
-from country_tests import *
-from linked_in_tests import *
-from search_page_tests import *
-from search_tests import *
-from template_tests import *
+from sarpaminfohub.contactlist.tests.contact_detail_page_tests import *
+from sarpaminfohub.contactlist.tests.country_tests import *
+from sarpaminfohub.contactlist.tests.linked_in_tests import *
+from sarpaminfohub.contactlist.tests.search_page_tests import *
+from sarpaminfohub.contactlist.tests.search_tests import *
+from sarpaminfohub.contactlist.tests.template_tests import *
